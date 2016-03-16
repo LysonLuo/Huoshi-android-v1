bb278362344dd9bdc3ec438cde6b52918aa33e9e
Lyson <lyson@bestapp.us>
2016-03-16 09:02:24 +0800

    集成友盟分享~

--- app/src/main/java/im/huoshi/HuoshiApplication.java
+++ app/src/main/java/im/huoshi/HuoshiApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 
 import com.bugtags.library.Bugtags;
 import com.bugtags.library.BugtagsOptions;
+import com.umeng.socialize.PlatformConfig;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -43,7 +44,12 @@ public class HuoshiApplication extends Application {
     }
 
     private void initAll() {
-
+        //微信 appid appsecret
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        //新浪微博 appkey appsecret
+        PlatformConfig.setSinaWeibo("3921700954","04b48b094faeb16683c32669824ebdad");
+        // QQ和Qzone appid appkey
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
 
     private void copyDataBase() {
---
+++ app/src/main/java/im/huoshi/wxapi/WXEntryActivity.java
@@ -0,0 +1,11 @@
+package im.huoshi.wxapi;
+
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by ntop on 15/9/4.
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
